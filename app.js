c9c633edac3499fceec1f635dec39ca9eb4c62c6
kwameltk <owusubright2002@gmail.com>
2022-05-09 11:13:04 -0700

    age calculator

---
+++ app.js
@@ -0,0 +1,13 @@
+var message = document.getElementById("message");
+
+function ageCalcu(){
+
+var birthYear = prompt("enter birth year")
+var presentYear = 2022;
+var combine = birthYear - presentYear ;
+var output = ("your are " + combine + " years old"); 
+
+alert(output)
+}
+ageCalcu()
+
